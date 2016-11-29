a49058841855536da8df4cfd6b4efe0638723e55
Clacson <cookiellacson@gmail.com>
2016-11-29 14:44:49 +0000

    9 tests passed

--- athlete.js
+++ athlete.js
@@ -1,6 +1,13 @@
 var athlete = {
   hydration: 100,
-  distanceCovered: 0
+  distanceCovered: 0,
+  run: function(){
+    if ( this.hydration <= 0 ) {
+      return;
+    }
+    this.hydration -= 10;
+    this.distanceCovered += 100;
+  }
 }
 
 module.exports = athlete;
--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -5,5 +5,26 @@ describe("Athlete", function(){
   it("should be hydrated to 100", function(){
     assert.equal(100, athlete.hydration);
   });
-  it("distance should start at 0 ")
+
+  it("distance covered should start at 0", function(){
+    assert.equal(0, athlete.distanceCovered);
+  });
+
+  it("athlete should run, increase distance, decrease hydration", function(){
+    athlete.run();
+    assert.equal(100, athlete.distanceCovered);
+    assert.equal(90, athlete.hydration);
+  });
+
+  it("athlete can't run when hydration is 0", function(){
+    for (var i = 0; i < 10; i++) {
+      athlete.run();}
+      athlete.run();
+    assert.equal(1000, athlete.distanceCovered);
+    assert.equal(0, athlete.hydration);
+  });
+
+  
+
+
 });
